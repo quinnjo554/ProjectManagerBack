67ae1010d382e3af2dba153f6f91b7260d836a66
quinnjo554 <quinnjohnson24@gmail.com>
2023-07-21 08:51:28 -0500

    added comment services

--- src/main/kotlin/com/example/demo/repository/CommentRepo.kt
+++ src/main/kotlin/com/example/demo/repository/CommentRepo.kt
@@ -1,7 +1,11 @@
 package com.example.demo.repository
 import com.example.demo.models.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CommentRepo : JpaRepository<Comment, Int>
+interface CommentRepo : JpaRepository<Comment, Int> {
+    fun findAllByTaskId(taskId: Int, pageable: Pageable): Page<Comment>?
+}
